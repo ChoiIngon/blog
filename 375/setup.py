79d9449652613ae619f091a5403acfe026eefc93
ingon choi <kukuta@gmail.com>
2021-09-29 01:31:22 +0900

    https://kukuta.tistory.com/375 [Python] C/C++ 연동 'extension' 모듈

--- 375/setup.py
+++ 375/setup.py
@@ -8,9 +8,10 @@ setup(
     name="PackageName",                 # 패키지 이름
     ext_modules=[
         Extension(
-            "c_module",
-            ["c_module_main.cpp"],
-            include_dirs = ["."],
+            "c_module",                 # 모듈 이름. 
+                                        # 초기화 함수 PyInit_<modulename>에서 <modulename>과 같아야 한다
+            ["c_module_main.cpp"],      # 빌드에 사용 될 C/C++ 파일
+            include_dirs = ["."],       # include 디렉토리 패스
         )
     ]
 )
