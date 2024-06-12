1c2afe56d1fec3c0ea02bdc38f5b9ddbbdf7c936
최인곤 <kukuta@nexon.co.kr>
2024-06-12 14:37:31 +0900

    https://kukuta.tistory.com/442 - [C++20] Concepts 활용

--- 442/main.cpp
+++ 442/main.cpp
@@ -5,14 +5,25 @@
 #include <string>
 #include <concepts>
 
-
 template <class T>
-struct RemoveModifier
+struct remove_modifier
 {
 	typedef std::remove_reference_t<T> non_ref_type;
 	typedef std::remove_all_extents_t<non_ref_type> non_extent_type;
 	typedef std::remove_pointer_t<non_extent_type> non_ptr_type;
 	typedef std::remove_const_t<non_ptr_type> type;
+};
+
+template <class T>
+struct is_basic_string
+{
+	static constexpr bool value = false;
+};
+
+template <class T>
+struct is_basic_string<std::basic_string<T>>
+{
+	static constexpr bool value = true;
 };
 
 template <class T>
@@ -25,7 +36,8 @@ concept StringType = requires(const T & t)
 template <class T>
 concept StringLike = requires(const T & t)
 {
-	*t;
+	requires std::negation_v<std::bool_constant<bool(is_basic_string<T>::value)>>;
+	requires std::is_convertible_v<T, std::basic_string_view<typename remove_modifier<T>::type>>;
 };
 
 template <class T> requires StringType<T>
@@ -94,30 +106,11 @@ size_t UTF8Length(const T& str)
 	return utf8_char_count;
 }
 
-
-template <class T> 
-requires StringLike<T>
+template <class T> requires StringLike<T>
 size_t UTF8Length(const T& str)
 {
-	typedef std::remove_reference_t<T> non_ref_type;
-	typedef std::remove_all_extents_t<non_ref_type> elmt_type;
-	typedef std::remove_pointer_t<elmt_type> non_ptr_type;
-	typedef std::remove_const_t<non_ptr_type> non_const_type;
-	typedef std::basic_string<non_const_type> string_type;
+	typedef std::basic_string<typename remove_modifier<T>::type> string_type;
 	return UTF8Length(string_type(str));
-}
-
-template <class T>
-concept ArrayType = requires(const T & t)
-{
-	requires std::is_pointer_v<T>;
-};
-
-template <class T> 
-requires ArrayType<T>
-void func(T t)
-{
-	std::cout << __func__ << std::endl;
 }
 
 int main()
@@ -125,29 +118,28 @@ int main()
 #ifdef _WIN32
     SetConsoleOutputCP(CP_UTF8);				// 윈도우 콘솔에 utf-8 문자열 출력하기 위해 필요
 #endif
-    auto s = u8"가나다 ABC abc 123 !@#";			// C++20 이전까지 utf-8로 인코딩된 const char* 타입
+    auto s = u8"\"가나다 ABC abc 123 !@#\"";			// C++20 이전까지 utf-8로 인코딩된 const char* 타입
 												// C++20 부터는 const char8_t* 타입
-	UTF8Length(s);
+	std::cout << "text:" << (char*)s << "/count:" << UTF8Length(s) << std::endl;
 
 	// 특수문자 '\'를 포함하고 있는 Raw string literals
-	auto r = u8R"(가나다 ABC abc 123 !@#\)";		// C++20 이전까지 utf-8로 인코딩된 const char* 타입
+	auto r = u8R"("가나다 ABC abc 123 !@#")";		// C++20 이전까지 utf-8로 인코딩된 const char* 타입
 												// C++20 부터는 const char8_t* 타입
-	UTF8Length(r);
+	std::cout << "text:" << (char*)r << "/count:" << UTF8Length(r) << std::endl;
 
 	using namespace std::string_literals;		// std::string 리터럴 오퍼레이터 활성화
 												// 접미사 s가 붙으면 string 객체를 의미한다.
-	auto S = u8"가나다 ABC abc 123 !@#"s;     	// C++20 이전까지 std::string, C++20 부터 std::u8string
-	UTF8Length(S);
-	
-	auto R = u8R"("가나다 ABC abc 123 !@#\")"s;	// C++20 이전까지 std::string, C++20 부터 std::u8string
-	UTF8Length(R);
+	auto S = u8"\"가나다 ABC abc 123 !@#\""s;     	// C++20 이전까지 std::string, C++20 부터 std::u8string
+	std::cout << "text:" << (char*)S.c_str() << "/count:" << UTF8Length(S) << std::endl;
 
-	char8_t a[] = u8"가나다 ABC abc 123 !@#";     	// C++20 이전까지 char[26], C++20 부터 char8_t[26]
-	UTF8Length(a);
-	
-	char8_t A[] = u8R"(가나다 ABC abc 123 !@#\n)";     	// C++20 이전까지 char[28], C++20 부터 char8_t[28]
-	UTF8Length(A);
+	auto R = u8R"("가나다 ABC abc 123 !@#")"s;	// C++20 이전까지 std::string, C++20 부터 std::u8string
+	std::cout << "text:" << (char*)R.c_str() << "/count:" << UTF8Length(R) << std::endl;
 
-	std::basic_string<RemoveModifier<std::u8string>> b;
+	char8_t a[] = u8"\"가나다 ABC abc 123 !@#\"";     	// C++20 이전까지 char[26], C++20 부터 char8_t[26]
+	std::cout << "text:" << (char*)a << "/count:" << UTF8Length(a) << std::endl;
+	
+	char8_t A[] = u8R"("가나다 ABC abc 123 !@#")";     	// C++20 이전까지 char[28], C++20 부터 char8_t[28]
+	std::cout << "text:" << (char*)A << "/count:" << UTF8Length(A) << std::endl;
+
 	return 0;
 }
